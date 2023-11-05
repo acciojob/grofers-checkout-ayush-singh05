b3a442f0ff457a74744cfaee30dd0abadd68d7ed
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-11-05 12:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+const price = document.getElementsByClassName("price");
 const getSum = () => {
 //Add your code here
-  
+	let val = 0;
+    let val1 = price[0].innerText;
+val += parseInt(price[0].innerText) 
+val += parseInt(price[1].innerText) 
+val += parseInt(price[2].innerText) 
+val += parseInt(price[3].innerText)
+val += parseInt(price[4].innerText) 
+
+const p = document.createElement("p");
+p.innerText = val;
+document.body.appendChild(p);
 };
 
 getSumBtn.addEventListener("click", getSum);
